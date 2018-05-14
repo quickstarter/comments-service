a02dd6f1a1a6534c7793ef98aed68c1f339702af
Jane Weesner <jane.e.weesner@gmail.com>
2018-05-14 16:21:18 -0700

    Add server clustering

--- client/src/clientIndex.jsx
+++ client/src/clientIndex.jsx
@@ -1,8 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
 
-window.React = React;
-window.ReactDOM = ReactDOM;
+import App from './components/App.jsx';
 window.Comments = App;
 
